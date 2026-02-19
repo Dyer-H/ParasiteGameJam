618da32f03e9509f7466e65673ef92d8d1ec8644
Carson <carson.engblom@gmail.com>
2026-02-18 20:14:55 -0700

    Added UI for the health/stamina bars

---
+++ Inventory/Scenes/health_bar.gd
@@ -0,0 +1,16 @@
+extends ProgressBar
+
+# Called when the node enters the scene tree for the first time.
+var stats=playerStats.new()
+
+func _ready() -> void:
+	var sb=StyleBoxFlat.new() # Create stylebox object
+	add_theme_stylebox_override("fill",sb) # Add a fill override to the stylebox
+	sb.bg_color=Color("ff596f") # Set fill color
+	stats.reset()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	self.value=stats.health # Set own value to health stat
+	
---
+++ Inventory/Scenes/stamina_bar.gd
@@ -0,0 +1,15 @@
+extends ProgressBar
+
+# Called when the node enters the scene tree for the first time.
+var stats=playerStats.new()
+
+func _ready() -> void:
+	var sb=StyleBoxFlat.new() # Create stylebox object
+	add_theme_stylebox_override("fill",sb) # Add a fill override to the stylebox
+	sb.bg_color=Color("59ff59") # Set fill color
+	stats.reset()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	self.value=stats.stamina # Set own value to stamina stat
