c52bbdacb185a1aebf20210e7c86c16d2fb00542
Dyer <dyerhunting@gmail.com>
2026-02-22 13:56:59 -0700

    Refactored to one main scene

---
+++ HUDLayer/Inventory/Scripts/NodeScripts/inventory_ui.gd
@@ -0,0 +1,22 @@
+#SCRIPT TIED TO INVENTORY UI CONTROL NODE
+#CONTAINS INVENTORY LOGIC
+extends Control
+
+@onready var inventoryPanels: Array = $CenterContainer/InventoryBackground/MarginContainer/GridContainer.get_children()
+@onready var wood = preload("res://HUDLayer/Inventory/ResourceInstances/wood.tres")
+
+#when it loads it shouldn't be visible
+func _ready() -> void:
+	self.visible = false
+	addWood()
+
+#when the "Toggle Inventory" button is pressed (mapped to i currently) -> show inventory
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("Toggle Inventory"):
+		if self.visible == true:
+			self.visible = false
+			return
+		self.visible = true
+		
+func addWood() -> void:
+	inventoryPanels[0].setItem(wood)
