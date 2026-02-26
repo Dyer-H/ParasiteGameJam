282bca2fd76d82b480252e394624c639555bfe00
Dyer <dyerhunting@gmail.com>
2026-02-26 15:34:42 -0700

    Added Code Comments to PlayerController

--- PlayerControl/Scripts/playerController.gd
+++ PlayerControl/Scripts/playerController.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 @onready var animated_sprite = $AnimatedSprite2D
 const SPEED = 80.0
 
+#literally just animation code -> no need to touch
+#unless of course animation also needs to be tied to delta param
+#which it probably will ngl
 func _process(_delta: float) -> void:
 	if Input.is_action_pressed("move_up") && Input.is_action_pressed("move_right"):
 		animated_sprite.play("walk_up_right")
@@ -23,9 +26,14 @@ func _process(_delta: float) -> void:
 	else:
 		animated_sprite.stop()
 
+#this function will have to pass delta to the next eventually
+#again because we don't want movement tied to framerate
 func _physics_process(_delta: float) -> void:
 	get_input()
-	
+
+#gets vector based on values set in project settings
+#can modify the speed using global parameter or in project settings
+#(probably should modify the one here (the global param))
 func get_input():
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = input_direction * SPEED
