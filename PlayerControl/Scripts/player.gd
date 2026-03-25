7687478d6b73890271a2c53309fd3a207787d832
Carson Engblom <carson.engblom@gmail.com>
2026-03-24 18:49:55 -0600

    Added player dash ('e' key)

--- PlayerControl/Scripts/player.gd
+++ PlayerControl/Scripts/player.gd
@@ -3,7 +3,9 @@ extends CharacterBody2D
 @export var projectile:PackedScene
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var healthbar = $"../HUD Layer/HP_STAM/HealthBar"
-const SPEED = 80.0 
+const DASH_DURATION=0.2 # Dash time (seconds)
+const DEFAULT_SPEED=80.0
+var SPEED = 80.0 
 
 #this function will have to pass delta to the next eventually
 #again because we don't want movement tied to framerate
@@ -17,7 +19,16 @@ func _physics_process(_delta: float) -> void:
 			newProjectile.global_position=global_position
 			newProjectile.look_at(get_global_mouse_position())
 			newProjectile.rotation_degrees+=(dev+(i*5-randi_range(-2,2))) # dev+(spread of each pellet)
-
+	if(Input.is_action_just_pressed("Dash")):
+		dash()
+		
+# Boosts the player speed for a set time (DASH_DURATION)
+func dash():
+	SPEED=(SPEED*3)
+	await get_tree().create_timer(DASH_DURATION).timeout # Create timer
+	SPEED=DEFAULT_SPEED
+	
+	
 #gets vector based on values set in project settings
 #discr etizes then moves that direction
 func get_input():
