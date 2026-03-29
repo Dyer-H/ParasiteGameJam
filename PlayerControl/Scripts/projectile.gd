b06963c66f86d88c43d4e4a5624853d7175c924a
CaptainJosh12345 <25joshuamcarr@gmail.com>
2026-03-29 12:31:40 -0600

    Big Enemy and Bullet Collision with Environment

--- PlayerControl/Scripts/projectile.gd
+++ PlayerControl/Scripts/projectile.gd
@@ -7,7 +7,7 @@ var bullet_range:float = 0.2 # Range is in seconds
 
 func _ready():
 	await get_tree().create_timer(bullet_range).timeout
-	queue_free()
+	free_node()
 	
 func _process(delta: float) -> void:
 	position+=transform.x*(delta*speed)
@@ -21,3 +21,7 @@ func get_crit():
 
 func free_node():
 	self.queue_free()
+
+func _on_body_entered(body: Node2D) -> void:
+	if "Walls" in body.name:
+		free_node()
