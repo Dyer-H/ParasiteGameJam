6c031e163cd59ea75087380f8313506bc652dcdb
Carson Engblom <carson.engblom@gmail.com>
2026-03-31 14:10:04 -0600

    Added initial graph structure for room traversal

---
+++ Global Scripts/Graph.gd
@@ -0,0 +1,25 @@
+extends Node
+
+var startNode:GraphNode
+var nodes:Array[GraphNode]
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var tempNode:GraphNode=GraphNode.new()
+	tempNode.id=0
+	startNode=tempNode
+	nodes.append(tempNode)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+func addNode(id:int,connection:int): # Connection is nodeID we're connecting to, id is the id of the new node
+	var newNode:GraphNode=GraphNode.new()
+	newNode.id=id
+	for node in nodes: # Find connecting node
+		if(node.id==connection):
+			newNode.connections.append(node)
+			node.connections.append(Node)
+	nodes.append(newNode)
---
+++ Global Scripts/GraphNode.gd
@@ -0,0 +1,15 @@
+extends Node
+
+var id:int
+var connections:Array[GraphNode]
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	id=0
+	connections=[]
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
