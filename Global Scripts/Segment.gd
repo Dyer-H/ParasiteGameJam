cd1c8af64264158ee489137667a0f8f741544b0f
Carson Engblom <carson.engblom@gmail.com>
2026-04-06 15:26:55 -0600

    Fixed room gen again

--- Global Scripts/Segment.gd
+++ Global Scripts/Segment.gd
@@ -20,22 +20,22 @@ func generateBranch(chance:int,maxDepth:int):
 	var depth=0
 	var firstNode=RoomGraphNode
 	firstNode.room=load("res://Map Scenes/lobby.tscn") # Lobby is first room of segment
-	firstNode.id=0
 	generateRoom(chance,0,maxDepth,firstNode,1)
 
 # Chance is how much the card drop % changes per room (key drop chance=chance/100)
 func generateRoom(chance:int,depth:int,maxDepth:int,previousRoom:RoomGraphNode,id:int):
 	if(depth!=maxDepth):
 		var newRoom=RoomGraphNode
-		newRoom.id=id
-		newRoom.parent=previousRoom
+		newRoom.room=rooms[randi_range(0,5)]
+		previousRoom.children.append(newRoom)
+		tree.nodes.append(newRoom)
+		tree.addNode(previousRoom,id-1)
 		var chancePercent=randi_range(chance,100)
 		if(chancePercent==100):
 			newRoom.hasKey=true
-		newRoom.room=rooms[randi_range(0,5)]
 		print("Generated room:")
 		print(newRoom.room)
-		print("ID: ",id," DEPTH: ",depth," PARENT: ",newRoom.parent)
+		print("ID: ",id," DEPTH: ",depth," PARENT: ",previousRoom.room)
 		
 		depth+=1
 		generateRoom(chance*2,depth,maxDepth,newRoom,id+1)
