a92dbd7def2d9bec98766c3594fd2ca747adf1e0
Dyer <dyerhunting@gmail.com>
2026-04-08 16:38:56 -0600

    Fixed dash completely

--- PlayerControl/Scripts/player.gd
+++ PlayerControl/Scripts/player.gd
@@ -11,46 +11,45 @@ var stats = preload("res://PlayerControl/Resources/player_stats.tres")
 const DASH_DURATION: float = 0.2 # Dash time (seconds)
 var stamRegen: bool = false
 var shot_cooldown: int = 0
-var dash_cooldown: int = 25
 var initial_speed: int
 var regen_rate: int
+var is_dashing: bool = false
 
 func _ready() -> void:
 	initial_speed = stats.player_speed
-	regen_rate = 10
+	regen_rate = 0
 	stats.changed.connect(_on_speed_changed)
 
 #this function will have to pass delta to the next eventually
 #again because we don't want movement tied to framerate
 func _physics_process(_delta: float) -> void:
-	get_input()
-	if (shot_cooldown > 0):
-		shot_cooldown -= 1
-	if (dash_cooldown > 0):
-		dash_cooldown -= 1
+	if (!is_dashing):
+		get_input()
+	else:
+		velocity = velocity * 3
+		move_and_slide()
+		velocity = velocity / 3
 	if (regen_rate > 0):
 		regen_rate -= 1
-	
-	if (dash_cooldown == 0):
-		stats.player_speed = initial_speed
-	if (stats.stamina < stats.max_stamina && stamRegen && regen_rate == 0):
+	else:
 		stats.set_stamina(stats.stamina + 1)
 		regen_rate = 10
 	if(Input.is_action_just_pressed("Shoot") && shot_cooldown == 0 && stats.curr_bullets > 0):
 		shoot()
-	if(Input.is_action_just_pressed("Dash") && dash_cooldown == 0):
+	if(Input.is_action_just_pressed("Dash")):
 		dash()
 
 # Boosts the player speed for a set time (DASH_DURATION)
 func dash():
-	if(stats.stamina > 33):
-		stamRegen=false
-		dash_cooldown = 10 #frames for dash
-		stats.set_stamina(stats.stamina - 33)
-		var initial_speed = stats.player_speed
-		stats.player_speed = initial_speed * 3
-		await get_tree().create_timer(stats.dash_regen).timeout
-		stamRegen=true
+	if is_dashing:
+		return
+	else:
+		if (stats.stamina > 33):
+			regen_rate = 100
+			stats.set_stamina(stats.stamina - 33)
+			is_dashing = true
+			await get_tree().create_timer(0.2).timeout
+			is_dashing = false
 	
 func shoot() -> void:
 	var dev=-15+randi_range(-5,5) # This is the starting deviation for the shot (degrees off from normal
